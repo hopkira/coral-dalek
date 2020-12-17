fbaf9e409e7e6e36f0ca15d318eba96ab0bc5670
Richard Hopkins <hopkira@googlemail.com>
2020-12-17 17:35:28 +0000

    Tweaks

--- recognition.py
+++ recognition.py
@@ -6,12 +6,12 @@ import imutils
 import cv2
 import time
 
-height = 480
-width = 640
+height = 720
+width = 1080
 resolution = (width, height)
 
 model = DetectionEngine("/usr/share/edgetpu/examples/models/ssd_mobilenet_v2_face_quant_postprocess_edgetpu.tflite")
-sp = dlib.shape_predictor("./shape_predictor_5_face_landmarks.dat")
+shape_pred = dlib.shape_predictor("./shape_predictor_5_face_landmarks.dat")
 facerec = dlib.face_recognition_model_v1("./dlib_face_recognition_resnet_model_v1.dat")
 
 win = dlib.image_window()
@@ -20,28 +20,27 @@ vs = VideoStream(src=0, usePiCamera = True, resolution=resolution, framerate = 6
 time.sleep(2.0)
 
 while True:
-    frame = vs.read()
-    # frame = imutils.resize(frame, width=500)
-    np_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = Image.fromarray(np_frame)
-    # win.set_image(np_frame)
-    results = model.detect_with_image(frame,
-        threshold = 0.9,
-        keep_aspect_ratio = True, 
-        relative_coord = False, 
-        top_k = 1)
-    for r in results:
-        box = r.bounding_box.flatten().astype("int")
-        (startX, startY, endX, endY) = box
-        d = dlib.rectangle(left=startX,right=endX,top=startY,bottom=endY)
-        shape = sp(np_frame, d)
-        # win.clear_overlay()
-        # win.add_overlay(d)
-        # win.add_overlay(shape)
-        face_chip = dlib.get_face_chip(np_frame, shape)
-        win.set_image(face_chip)
-        #face_descriptor = facerec.compute_face_descriptor(face_chip)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
-vs.stop()
+    try:
+        cam_frame = vs.read()
+        # frame = imutils.resize(frame, width=500)
+        np_frame = cv2.cvtColor(cam_frame, cv2.COLOR_BGR2RGB)
+        img_frame = Image.fromarray(np_frame)
+        # win.set_image(np_frame)
+        face_list = model.detect_with_image(img_frame,
+            threshold = 0.7,
+            keep_aspect_ratio = True, 
+            relative_coord = False, 
+            top_k = 1)
+        for face in face_list:
+            face_box = face.bounding_box.flatten().astype("int")
+            (startX, startY, endX, endY) = face_box
+            box = dlib.rectangle(left=startX,right=endX,top=startY,bottom=endY)
+            shape = shape_pred(np_frame, box)
+            # win.clear_overlay()
+            # win.add_overlay(d)
+            # win.add_overlay(shape)
+            face_img = dlib.get_face_chip(np_frame, shape)
+            win.set_image(face_img)
+            #face_descriptor = facerec.compute_face_descriptor(face_chip)
+    except KeyboardInterrupt:
+        vs.stop()
