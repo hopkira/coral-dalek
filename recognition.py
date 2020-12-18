ba23320c4c6f5d43431657b869e8e2dde9a5d1fe
Richard Hopkins <hopkira@googlemail.com>
2020-12-18 17:35:47 +0000

    Calculate position

--- recognition.py
+++ recognition.py
@@ -5,10 +5,17 @@ import dlib
 import imutils
 import cv2
 import time
+import math
 
-height = 1080
-width = 1920
-resolution = (width, height)
+HEIGHT = 1080 # pixels
+WIDTH = 1920 # pixels
+RESOLUTION = (WIDTH, HEIGHT)
+
+SENSOR_WIDTH = 6.3 # sensor width in mm
+LENS_FOCAL_LENGTH = 6.0 # sensor focal length in mm
+EYE_DISTANCE = 82.0 # distance between edges of eyes in mmm
+
+PIX_TO_M = LENS_FOCAL_LENGTH * EYE_DISTANCE * HEIGHT / SENSOR_WIDTH / 1000.0
 
 #white = dlib.rgb_pixel(255,255,255)
 
@@ -19,8 +26,23 @@ facerec = dlib.face_recognition_model_v1("./dlib_face_recognition_resnet_model_v
 #win = dlib.image_window()
 #win.set_title("Homing Camera")
 
-vs = VideoStream(src=0, usePiCamera = True, resolution=resolution, framerate = 30).start()
-time.sleep(2.0)
+vs = VideoStream(src=0, usePiCamera = True, resolution=RESOLUTION, framerate = 30).start()
+
+time.sleep(2.0) # wait for camera feed to start
+
+def calc_position(eye_width, eye_offset):
+    position = dict{}
+    offset = pixel_to_m(eye_offset)
+    position["angle"]= math.atan(offset/dist)
+    position["dist"] = pixel_to_m(eye_width)
+    return dict(angle = math.atan(offset/dist),
+                dist = pixel_to_dist(eye_width))
+
+def pixel_to_m(pixels):
+    if pixels:
+        return PIX_TO_M / pixels
+    else:
+        return 10.0
 
 while True:
     try:
@@ -39,23 +61,25 @@ while True:
             (startX, startY, endX, endY) = face_box
             box = dlib.rectangle(left=startX,right=endX,top=startY,bottom=endY)
             shape = shape_pred(np_frame, box)
-            left_x = shape.part(0).x
-            right_x = shape.part(3).x
-            left_y = shape.part(0).y
-            right_y = shape.part(3).y
-            distance = ((((right_x - left_x )**2) + ((right_y - left_y)**2) )**0.5)
-            bearing = (width / 2) - ((right_x + left_x) /2)
-            text = "Distance: " + str(int(distance)) + " Bearing: " + str(int(bearing))
-            print(text)
-            #win.clear_overlay()
-            #win.add_overlay(shape,white)
-            #win.add_overlay(box,white)
-            # face_img = dlib.get_face_chip(np_frame, shape)
-            # win.set_image(face_img)
-            # face_descriptor = facerec.compute_face_descriptor(face_chip)
-            # np.linalg.norm(known_faces - face, axis=1)
-            # return np.linalg.norm(face_encodings - face_to_compare, axis=1)
-            # dlib.full_object_detection, idx:
+            if shape:
+                left_x = shape.part(0).x
+                right_x = shape.part(3).x
+                left_y = shape.part(0).y
+                right_y = shape.part(3).y
+                eye_width = ((((right_x - left_x )**2) + ((right_y - left_y)**2) )**0.5)
+                eye_offset = (width / 2) - ((right_x + left_x) /2)
+                position = calc_position(eye_width=eye_width, eye_offset=eye_offset)
+                degrees = math.degrees(position['angle'])
+                print 'Distance = %.2fm Angle = %.2f degrees' % (position['dist'], degrees)
+                #win.clear_overlay()
+                #win.add_overlay(shape,white)
+                #win.add_overlay(box,white)
+                # face_img = dlib.get_face_chip(np_frame, shape)
+                # win.set_image(face_img)
+                # face_descriptor = facerec.compute_face_descriptor(face_chip)
+                # np.linalg.norm(known_faces - face, axis=1)
+                # return np.linalg.norm(face_encodings - face_to_compare, axis=1)
+                # dlib.full_object_detection, idx:
     except KeyboardInterrupt:
         vs.stop()
         print("Stopped video stream")
