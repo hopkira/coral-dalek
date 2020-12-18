6614b0738b3b0a0c5e6b5f3c52391369c60ee137
Richard Hopkins <hopkira@googlemail.com>
2020-12-18 19:29:02 +0000

    Show face chip

--- recognition.py
+++ recognition.py
@@ -24,8 +24,8 @@ model = DetectionEngine("/usr/share/edgetpu/examples/models/ssd_mobilenet_v2_fac
 shape_pred = dlib.shape_predictor("./shape_predictor_5_face_landmarks.dat")
 facerec = dlib.face_recognition_model_v1("./dlib_face_recognition_resnet_model_v1.dat")
 
-#win = dlib.image_window()
-#win.set_title("Homing Camera")
+win = dlib.image_window()
+win.set_title("Face Chip")
 
 vs = VideoStream(src=0, usePiCamera = True, resolution=RESOLUTION, framerate = 30).start()
 
@@ -34,7 +34,6 @@ time.sleep(2.0) # wait for camera feed to start
 def calc_position(eye_width, eye_offset):
     dist = PIX_TO_M / eye_width
     offset = eye_offset / eye_width * EYE_DISTANCE / 1000
-    print('Offset = %.2fm Dist = %.2f m Atan2 = %.4f' % (offset, dist, math.atan2(offset,dist)))
     return dict(angle = math.atan2(offset,dist), dist = dist)
 
 while True:
@@ -55,25 +54,23 @@ while True:
             box = dlib.rectangle(left=startX,right=endX,top=startY,bottom=endY)
             shape = shape_pred(np_frame, box)
             if shape:
+                # win.clear_overlay()
+                # win.add_overlay(shape,white)
+                # win.add_overlay(box,white)
+                face_img = dlib.get_face_chip(np_frame, shape)
+                win.set_image(face_img)
+                face_descriptor = facerec.compute_face_descriptor(face_chip)
+                # np.linalg.norm(known_faces - face, axis=1)
+                # return np.linalg.norm(face_encodings - face_to_compare, axis=1)
+                # dlib.full_object_detection, idx:
                 left_x = shape.part(0).x
                 right_x = shape.part(3).x
                 left_y = shape.part(0).y
                 right_y = shape.part(3).y
                 eye_width = ((((right_x - left_x )**2) + ((right_y - left_y)**2) )**0.5)
                 eye_offset = ((right_x + left_x) /2) - (WIDTH / 2)
-                print(eye_offset)
                 position = calc_position(eye_width=eye_width, eye_offset=eye_offset)
-                #print('Distance = %.2fpx Offset = %.2f px' % (eye_width, eye_offset))
-                #print('Distance = %.2fm Angle = %.2f radians' % (position['dist'], position['angle']))
-                #win.clear_overlay()
-                #win.add_overlay(shape,white)
-                #win.add_overlay(box,white)
-                # face_img = dlib.get_face_chip(np_frame, shape)
-                # win.set_image(face_img)
-                # face_descriptor = facerec.compute_face_descriptor(face_chip)
-                # np.linalg.norm(known_faces - face, axis=1)
-                # return np.linalg.norm(face_encodings - face_to_compare, axis=1)
-                # dlib.full_object_detection, idx:
+                print('Distance = %.2fm Angle = %.2f radians' % (position['dist'], position['angle']))
     except KeyboardInterrupt:
         vs.stop()
         print("Stopped video stream")
