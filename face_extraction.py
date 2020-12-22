a7d2679239de0b59a569578e89a842466c075c4b
Richard Hopkins <hopkira@googlemail.com>
2020-12-22 19:58:19 +0000

    Update face_extraction.py

--- face_extraction.py
+++ face_extraction.py
@@ -6,6 +6,7 @@ facerec = dlib.face_recognition_model_v1("./dlib_face_recognition_resnet_model_v
 
 def extract_face_data(np_frame,face):
     width = np_frame.shape[0]
+    print(np_frame.shape)
     face_box = face.bounding_box.flatten().astype("int")
     (startX, startY, endX, endY) = face_box
     box = dlib.rectangle(left=startX,right=endX,top=startY,bottom=endY)
