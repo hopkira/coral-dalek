9d1b6467bef46672e6b6d4484848819e38dfc36c
Richard Hopkins <hopkira@googlemail.com>
2020-12-22 22:46:16 +0000

    Update memory.py

--- memory.py
+++ memory.py
@@ -10,13 +10,12 @@ with picamera.PiCamera(sensor_mode = 2) as camera:
         try:
             start = time.perf_counter()
             camera.capture('/var/tmp/capture.jpg', 'rgb')
-            np_img = cv2.imread('/var/tmp/capture.jpg', cv2.IMREAD_COLOR)
-            # np_img = cv2.cvtColor(np_img, cv2.COLOR_BGR2RGB)
+            image = Image.open('/var/tmp/capture.jpg')
+            np_img = np.asarray(image)
             print(f'Captured {np_img.shape[1]}x{np_img.shape[0]}x{np_img.shape[2]} numpy array')
-            img = Image.fromarray(np_img)
             np_img.truncate(0)
             end = time.perf_counter()
             print(f'Frame taken in {(end-start)*1000} ms')
         except KeyError:
             camera.close()
-            system.exit("Numpy capture complete")
+            sys.exit("Numpy capture complete")
