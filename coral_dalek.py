34d7d3f34647daa32f9507f76ce90a0c5a725bf5
Richard Hopkins <hopkira@googlemail.com>
2021-01-03 17:17:12 +0000

    Update coral_dalek.py

--- coral_dalek.py
+++ coral_dalek.py
@@ -74,8 +74,8 @@ while True:
                 face_list.update_list(face_data)
         for face in face_list.faces:
             print('%s is at %.2fm and a bearing of %.2f radians' % (face['name'], face['position']['z_dist'], face['position']['h_angle']))
-            win.set_image(face['face_chip_img'])
-            win.set_title("XXX")
+            # win.set_image(face['face_chip_img'])
+            # win.set_title("XXX")
 
     except KeyboardInterrupt:
         vs.stop()
