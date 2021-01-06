784454974e7021e56b05e33e6a4ecb6af1854198
Richard Hopkins <hopkira@googlemail.com>
2021-01-06 18:33:43 +0000

    Bug fix

--- servotest.py
+++ servotest.py
@@ -2,10 +2,7 @@ from board import SCL, SDA
 import busio
 from adafruit_pca9685 import PCA9685
 
-# create iris servo
-i2c_bus = busio.I2C(SCL, SDA)
-pca = PCA9685(i2c_bus)
-pca.frequency = 50
+
 
 # Servo Channels
 IRIS_SERVO = 4
@@ -13,19 +10,22 @@ DOME_LIGHTS = 1
 IRIS_LIGHT = 2
 HOVER_LIGHTS = 3
 
+FREQUENCY = 50
+PERIOD = 1.0 / FREQUENCY * 1000.0
+
+# create iris servo
+i2c_bus = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c_bus)
+pca.frequency = FREQUENCY
+
 # Convenience Servo Values
-ON = 0xffff
-OPEN = 0x7FFF
-MID = 0x7FFF
-CLOSED = 0x7FFF
-OFF = 0x0000
+ON = 1.0
+OPEN = 1.0
+MID = 0.5
+CLOSED = 0.0
+OFF = 0.0
 
 def dalek_servo(channel,value):
-    value = ON * value
-    pca.channels[channel].duty_cycle = value
-
-def servo_state(instr_list):
-    for instr in instr_list:
-        dalek_servo(instr[0],instr[1])
-
-servo_state([IRIS_SERVO, MID])
+    value =+ 1
+    duty_cycle = int(value / PERIOD / 65535.0)
+    pca.channels[channel].duty_cycle = duty_cycle
