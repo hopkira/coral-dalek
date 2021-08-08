99ed1b5d16ba8bbe2b4d279a3bac4f7e102e0574
Richard Hopkins <hopkira@googlemail.com>
2021-08-08 20:37:43 +0100

    Create flash_lights.py

---
+++ flash_lights.py
@@ -0,0 +1,43 @@
+import pyaudio
+import threading
+
+# Vales to control whether dome lights are on or off
+VOL_MIN = 5000
+VOL_MAX = 20000
+
+# Sets up a daemon thread to flash lights in line with sound
+def flash_dome_lights():
+    ''' Daemon thread to flash lights based on microphone noise '''
+
+    while True:
+        try:
+            data = np.frombuffer(stream.read(CHUNK, False),dtype=np.int16)
+            vol = abs(int(np.average(np.abs(data))))
+            print(vol)
+            if vol > VOL_MIN:
+                vol = vol - VOL_MIN
+            else:
+                vol = 0
+            vol = vol * ON / VOL_MAX
+            if vol > ON:
+                vol =  ON
+            dalek_light(DOME_LIGHTS, vol / ON)
+        except ValueError:
+            print ("Volume out of range: " + vol)
+
+print("Starting audio thread...")
+p = pyaudio.PyAudio()
+stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
+              frames_per_buffer=CHUNK, input_device_index=1)
+domeLightsThread = Thread(target=flash_dome_lights, daemon=True)
+domeLightsThread.start()
+print("Audio thread started...")
+
+try:
+    while True:
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    print("Lights stopped by user.")
