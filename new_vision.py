8d5f08ad1e6a44fcd2b459774fc4f4be2f299462
Richard Hopkins <hopkira@googlemail.com>
2021-08-11 00:07:14 +0100

    Update new_vision.py

--- new_vision.py
+++ new_vision.py
@@ -12,9 +12,22 @@ from pycoral.utils.edgetpu import make_interpreter
 from imutils.video import VideoStream
 from PIL import Image, ImageDraw
 
+# import local helper classes
+from faceextractor import FaceDataExtractor
+from recognizer import FaceRecognizer
+
 print("Loading face detection engine...")
 interpreter = make_interpreter("/home/pi/coral-dalek/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite")
 interpreter.allocate_tensors()
+
+print("Loading face landmark detection engine...")
+shape_pred = dlib.shape_predictor("./shape_predictor_5_face_landmarks.dat")
+face_ext = FaceDataExtractor()
+print("Loading face recognition engine...")
+facerec = dlib.face_recognition_model_v1("./dlib_face_recognition_resnet_model_v1.dat")
+face_recog = FaceRecognizer()
+
+print("Starting video capture")
 
 vc = cv2.VideoCapture(0)
 if not vc.isOpened():
@@ -26,6 +39,8 @@ while True:
     if not ret:
         print("No frame received from camera; exiting...")
         break
+    # Convert frame from color_coverted = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     image = Image.fromarray(frame)
     _, scale = common.set_resized_input(
         interpreter, image.size, lambda size: image.resize(size, Image.ANTIALIAS))
@@ -35,7 +50,9 @@ while True:
     for face in face_box_list:
         bbox = face.bbox
         draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)], outline='white')
+        # line 556 in Dalek State
     displayImage = np.asarray(image)
+    displayImage = cv2.cvtColor(displayImage, cv2.COLOR_RGB2BGR)
     cv2.imshow('Object Detection', displayImage)
     if cv2.waitKey(1) == ord('q'):
         break
