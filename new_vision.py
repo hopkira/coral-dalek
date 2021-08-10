433e90aec90c999baf369501fb7cf78b8a3ba0f5
Richard Hopkins <hopkira@googlemail.com>
2021-08-10 22:09:04 +0100

    Update new_vision.py

--- new_vision.py
+++ new_vision.py
@@ -50,7 +50,7 @@ while True:
     #    bbox = face.bbox
     #    draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)], outline='white')
     #displayImage = np.asarray(image)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.imshow('Object Detection', gray)
     if cv2.waitKey(1) == ord('q'):
         break
