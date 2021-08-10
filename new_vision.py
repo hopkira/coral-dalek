a166196599bcfdd5bb8dd0579d7c801f3704f2f7
Richard Hopkins <hopkira@googlemail.com>
2021-08-10 21:12:25 +0100

    Update new_vision.py

--- new_vision.py
+++ new_vision.py
@@ -25,30 +25,30 @@ interpreter.allocate_tensors()
 print("Starting video stream...")
 vs = VideoStream(src=0,
                  usePiCamera = False,
-                 resolution=RESOLUTION,
+                 resolution = RESOLUTION,
                  framerate = FRAMERATE).start()
 
 print("Waiting for camera feed to start...")
 time.sleep(1.0) # wait for camera feed to start
-print("Opening camera stream...")
+print("Camera stream open...")
 
 while True:
     cam_frame = vs.read()
     # np_frame = cv2.cvtColor(cam_frame, cv2.COLOR_BGR2RGB)
     image = Image.fromarray(cam_frame)
     
-    _, scale = common.set_resized_input(
-        interpreter, image.size, lambda size: image.resize(size, Image.ANTIALIAS))
+    #_, scale = common.set_resized_input(
+    #    interpreter, image.size, lambda size: image.resize(size, Image.ANTIALIAS))
 
-    interpreter.invoke()
+    #interpreter.invoke()
 
-    face_box_list = detect.get_objects(interpreter, 0.7, scale)
+    #face_box_list = detect.get_objects(interpreter, 0.7, scale)
 
-    draw = ImageDraw.Draw(image)
+    #draw = ImageDraw.Draw(image)
 
-    for face in face_box_list:
-        bbox = face.bbox
-        draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)], outline='white')
+    #for face in face_box_list:
+    #    bbox = face.bbox
+    #    draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)], outline='white')
 
     displayImage = np.asarray(image)
-    cv2.imshow('Coral Live Object Detection', displayImage)
+    cv2.imshow('Object Detection', displayImage)
