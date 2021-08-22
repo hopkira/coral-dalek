3c8e8e0df6596c88b501d4c4aea9bc5ba5547a92
Richard Hopkins <hopkira@googlemail.com>
2021-08-22 14:57:54 +0100

    Update new_coral_dalek.py

--- new_coral_dalek.py
+++ new_coral_dalek.py
@@ -174,8 +174,8 @@ if args['output']:
         spl = UnivariateSpline(x, y)
         return spl(range(256))
 
-    inc_col = create_transform([0, 64, 128, 192, 256],[150, 175, 200, 225, 256])
-    dec_col = create_transform([0, 64, 128, 192, 256],[28, 64, 90, 110, 128])
+    inc_col = create_transform([0, 64, 128, 192, 255],[150, 175, 200, 225, 255])
+    dec_col = create_transform([0, 64, 128, 192, 255],[28, 64, 90, 110, 128])
 
 print("Starting video stream...")
 
@@ -740,43 +740,43 @@ for label in labels:
     known_people.append(Person(label))
 
 last_message = ""
-#client = mqtt.Client("dalek-python")
-#client.connect("localhost")
+client = mqtt.Client("dalek-python")
+client.connect("localhost")
 
-# client.publish("test/message","did you get this?")
-#def on_message(message):
-#    """
-#    Enables the Dalek to receive a message from an Epruino Watch via
-#    MQTT over Bluetooth (BLE) to place it into active or inactive States
-#    """
-#    global last_message
-#    payload = str(message.payload.decode("utf-8"))
-#    if payload != last_message:
-#        last_message = payload
-#        payload = payload.replace('"', "")
-#        command = payload.split(",")
-#        print(command)
-#        if command[1] == "Dale" and command[2] == "face" and command[3] == "on":
-#            dalek.on_event('waiting')
-#        if command[1] == "Dale" and command[2] == "face" and command[3] == "off":
-#            dalek.on_event('silent')
-#    else:
-#        dalek.on_event('unknown')
-#
-#client.on_message = on_message        # attach function to callback
-#client.subscribe("/ble/advertise/d3:fe:97:d2:d1:9e/espruino/#")
+#client.publish("test/message","did you get this?")
+def on_message(message):
+    """
+    Enables the Dalek to receive a message from an Epruino Watch via
+    MQTT over Bluetooth (BLE) to place it into active or inactive States
+    """
+    global last_message
+    payload = str(message.payload.decode("utf-8"))
+    if payload != last_message:
+        last_message = payload
+        payload = payload.replace('"', "")
+        command = payload.split(",")
+        print(command)
+        if command[1] == "Dale" and command[2] == "face" and command[3] == "on":
+            dalek.on_event('waiting')
+        if command[1] == "Dale" and command[2] == "face" and command[3] == "off":
+            dalek.on_event('silent')
+    else:
+        dalek.on_event('unknown')
+
+client.on_message = on_message        # attach function to callback
+client.subscribe("/ble/advertise/d3:fe:97:d2:d1:9e/espruino/#")
 
 try:
     while True:
         dalek.run()
 #        time.sleep(0.1)
-#        client.loop(0.1)
+        client.loop(0.1)
 except KeyboardInterrupt:
     pca.deinit()
     stream.stop_stream()
     stream.close()
     p.terminate()
-#    client.loop_stop()
+    client.loop_stop()
     vc.release()
     cv2.destroyAllWindows()
     print("Dalek halted by CTRL+C")
