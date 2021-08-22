4f4817f53700918101057b88dfd81f5f057cdcd8
Richard Hopkins <hopkira@googlemail.com>
2021-08-23 00:15:17 +0100

    Update watchtext.py

--- watchtext.py
+++ watchtext.py
@@ -11,10 +11,12 @@ def on_message(client, userdata, message):
     MQTT over Bluetooth (BLE) to place it into active or inactive States
     """
     global last_message
+    
     payload = str(message.payload.decode("utf-8"))
     if payload != last_message:
         last_message = payload
-        print(payload)
+        event = payload[2:].lower()
+        dalek.on_event(event)
         #if command[1] == "Dale" and command[2] == "face" and command[3] == "on":
         #    dalek.on_event('waiting')
         #if command[1] == "Dale" and command[2] == "face" and command[3] == "off":
@@ -23,7 +25,7 @@ def on_message(client, userdata, message):
         #dalek.on_event('unknown')
 
 client.on_message = on_message        # attach function to callback
-client.subscribe("/ble/advertise/d3:fe:97:d2:d1:9e/espruino/#")
+client.subscribe("/ble/advertise/d3:fe:97:d2:d1:9e/espruino/m")
 
 while True:
     try:
